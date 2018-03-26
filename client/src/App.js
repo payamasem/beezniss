c405b0fe5ef14aba850e686239fd549daebd338e
ehler <ehlerorngard@calvisitor-10-105-129-160.calvisitor.1918.berkeley.edu>
2018-03-26 10:42:04 -0700

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,14 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 const App = () => (
-  <Router>
     <div>
       <Nav />
-      <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
+      <TaskManager />
+      <SalesTracker />
+      <NewsScraper />
+      <Marketing />
+      <Messenger />
     </div>
-  </Router>
 );
 
 export default App;
