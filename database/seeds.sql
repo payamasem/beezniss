d547e12ed5d43a542bc9c1b748f83604a69d88b8
NI GUO <guoni0909@gmail.com>
2018-03-25 17:59:44 -0700

    hard coded the datas in seeds and schema files

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,35 +1,52 @@
-INSERT INTO Parents
-    (first_name, last_name, phone_number, parentMedConsent)
+INSERT INTO user
+    (first_name, last_name, )
 VALUES
-    ("Kobe", "Bryant", "4087659877", true),
-    ("LeBron", "James", "6509234561", false),
-    ("Steph", "Curry", "3109877885", false),
-    ("Kevin", "Durant", "4154439928", true),
-    ("Kevin", "Hart", "7652109887", false),
-    ("Draymond", "Green", "6895432167", false),
-    ("Klay", "Thompson", "8285432098", true),
-    ("Lonzo", "Ball", "2129076342", true),
-    ("Serena", "Williams", "6198777390", false),
-    ("Kate", "Middleton", "7279017543", false);
+    ("Kobe", "Bryant"),
+    ("LeBron", "James"),
+    ("Steph", "Curry"),
+    ("Kevin", "Durant"),
+    ("Kevin", "Hart"),
+    ("Draymond", "Green"),
+    ("Klay", "Thompson"),
+    ("Lonzo", "Ball"),
+    ("Serena", "Williams"),
+    ("Kate", "Middleton");
 
-INSERT INTO Staffs
-    (first_name, last_name, phone_number, email, username)
+INSERT INTO task
+    (heading, description, due_date, checklist_item)
 VALUES
-    ("Johnny", "Bravo", "5555763427", "jbravo@school.edu", "jbravo"),
-    ("Patty", "Cake", "3411433414", "pcake@school.edu", "pcake"),
-    ("Rocky", "Balboa", "8085432789", "stallion@school.edu", "stallion"),
-    ("Tony", "Stark", "4145647889", "ironman@school.edu", "ironman"),
-    ("Lavar", "Ball", "9095443217", "bbb@school.edu", "bbb"),
-    ("Tom", "Brady", "3237564326", "goat@school.edu", "goat");
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-20"),
+    ("You may need to do something", "maybe you need to check the To do list now", "2018-4-22"),
+    ("You have to do something", "maybe you need to check the To do list", "2018-4-20"),
+    ("You need to do something again", "maybe you need to check the To do list", "2018-4-10"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-26"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-30"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-21"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-24"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-25"),
+    ("You need to do something", "maybe you need to check the To do list", "2018-4-4");
 
-INSERT INTO Users
-    (username, password)
+INSERT INTO checklist
+    (title, completed, due_date)
 VALUES
-    ( "jbravo", "12345"),
-    ("pcake", "12345"),
-    ("stallion", "12345"),
-    ("ironman", "12345"),
-    ("bbb", "12345"),
-    ("goat", "12345");
+    ("maybe you need to check the To do list", "2018-4-20"),
+    ( "maybe you need to check the To do list now", "2018-4-22"),
+    ("maybe you need to check the To do list", "2018-4-20"),
+    ("maybe you need to check the To do list", "2018-4-20"),
+    ( "maybe you need to check the To do list now", "2018-4-22"),
+    ("maybe you need to check the To do list", "2018-4-20"),
+    ("maybe you need to check the To do list", "2018-4-20"),
+    ( "maybe you need to check the To do list now", "2018-4-22"),
+    ("maybe you need to check the To do list", "2018-4-20");
 
-
+INSERT INTO project
+    (title, due_date, checklist_item)
+VALUES
+    ("project 1", "2018-4-20"),
+    ("project 2", "2018-4-20"),
+    ("project 3", "2018-4-20"),
+    ("project 4", "2018-4-20"),
+    ("project 5", "2018-4-20"),
+    ("project 6", "2018-4-20"),
+    ("project 5", "2018-4-20"),
+    ("project 6", "2018-4-20");
