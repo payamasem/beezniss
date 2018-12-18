1721ff9adb8d3fbd85a811a62cc6b83de92d32ed
ehler <ehlerorngard@ehlers-MacBook-2.local>
2018-12-17 17:36:09 -0800

    heroku.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(routes);
 app.use(passport.initialize());
 app.use(passport.session());
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, function() {
 	  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 	});
